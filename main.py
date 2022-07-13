d3fcdb71ff22fe6f42b8c6a1bac98deb6c2c3137
NameUser5 <sdaut001@fiu.edu>
2022-07-13 11:38:54 -0400

    Skeleton for the nestled if statements for three choices.
    Pycharm says it cannot run my program. I think I deleted the .idea and venv files in error.
    
    Will troubleshoot and probably wipe my Git repo clean and start again with a different Pycharm file (without the sprites).

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
 import random
 
-user = input("")
+'''Placeholder for GUI choice'''
+user = input("Choose: R - P - S").lower()
+rock = "r"
+paper = "p"
+scissors = "s"
+
+gambits = [rock,paper,scissors]
+
+cpu_choice = random.choice(gambits)
+
+print(cpu_choice)
+
+draw = 0
+win = 1
+lose = -1
+
+# Choices #
+if input == rock:
+    if cpu_choice == rock:
+        status = draw
+    elif cpu_choice == paper:
+        status = lose
+    elif cpu_choice == scissors:
+        status = win
+elif input == paper:
+    if cpu_choice == rock:
+        status = win
+    elif cpu_choice == paper:
+        status = draw
+    elif cpu_choice == scissors:
+        status = lose
+elif input == scissors:
+    if cpu_choice == rock:
+        status = lose
+    elif cpu_choice == paper:
+        status = win
+    elif cpu_choice == scissors:
+        status = draw
